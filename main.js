abef8dc067700f5728319657433c7f681b07d513
Lukas <lukaskoller6@gmail.com>
2017-03-20 20:26:09 -0700

    testing stuff

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 /*<Firebase Code*/
 
-    /*// Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCYcgykH7P7lv55TXXttWyfAFWdcEEDyFA",
-        authDomain: "shopping-list-629b4.firebaseapp.com",
-        databaseURL: "https://shopping-list-629b4.firebaseio.com",
-        storageBucket: "shopping-list-629b4.appspot.com",
-        messagingSenderId: "369869077853"
-    };
-    firebase.initializeApp(config);*/
-
     // Track the UID of the current user.
     var currentUid = null;
     firebase.auth().onAuthStateChanged(function(user) {
